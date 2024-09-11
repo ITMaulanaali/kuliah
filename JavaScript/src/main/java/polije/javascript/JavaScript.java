a4f96133c609e6bb3a1d18601e6d727f3a172624
ITMaulanaali <it.maulanaali@gmail.com>
2024-09-11 22:41:34 +0700

    .

---
+++ JavaScript/src/main/java/polije/javascript/JavaScript.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package polije.javascript;
+
+/**
+ *
+ * @author lan
+ */
+public class JavaScript {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
