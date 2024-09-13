6fdb3cf5c352af36fd9bc80e7cfc19219a069a35
ITMaulanaali <it.maulanaali@gmail.com>
2024-09-13 15:35:33 +0700

    contoh syntax for loop

---
+++ c++/for.cpp
@@ -0,0 +1,19 @@
+/* for loop adalah perulangan yang disesuaikan sendiri atau syaratnya bisa lebih complex
+contoh syntaxnya
+
+for(inisialisasi, kondisi, increment){
+	statement
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	for(int counter = 1, counter << 10, counter ++){
+		cout << counter << endl;
+	}
+	cin.get();
+	return 0;
+}
