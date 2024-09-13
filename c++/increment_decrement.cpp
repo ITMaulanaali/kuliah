147a35081080b6eb076c4077c92073b1c2890ef1
ITMaulanaali <it.maulanaali@gmail.com>
2024-09-13 10:37:05 +0700

    postincrement a++ preincrement ++a

---
+++ c++/increment_decrement.cpp
@@ -0,0 +1,10 @@
+/*	Increment dan decrement itu biasanya dan digunakan untuk aritmatika
+dengan simbol ++ yang berarti menambah 1(increment) dan -- yang berarti -1(decrement).
+
+	Ada 2 jeni increment dan decrement yaitu:
+	postincrement
+	a++ //a dijalankan dulu kemudian di tambah 1
+
+	preincrement
+	++a //tambahan dulu 1 ke a kemudian di print atau jalankan variable
+*/
