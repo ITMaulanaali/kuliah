e66e1b8319261dc1c676d80f7f4289cb6d8b2b94
ITMaulanaali <it.maulanaali@gmail.com>
2024-09-15 00:17:18 +0700

    membuat alias pada typedata

---
+++ c++/typedef.cpp
@@ -0,0 +1,8 @@
+//typedef hanyalah membuat sebuah type data memiliki alias nama yang tentunya jrang atau tidak pernah digunakan.
+
+int main(){
+	typedef int I;
+
+	I a = 5;
+	cout << a << endl; //hasilnya adalah 5 denga type data integer
+}
