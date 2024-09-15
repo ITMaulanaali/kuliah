c2ac6b3f333128fef85af4b053d29c508fad2119
ITMaulanaali <it.maulanaali@gmail.com>
2024-09-15 18:27:03 +0700

    membuat object

---
+++ c++/oop/berbagai_macam_cara_identifier_dan_atau_inisialisasi+object_pertama_kali.cpp
@@ -0,0 +1,62 @@
+/*Object yang pertama kali dibuat ini sangat penting dari pada sekedar memanggil object dengan methodnya.
+Pembuatan object atau identifier serta inisialisasinya pada c++ ini akan mempengaruhi constructor nantinya*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class DenganConstructor{
+    public:
+        string data;
+
+        DenganConstructor(const char* data){
+            DenganConstructor::data = data;
+        }
+
+        void show(){
+            cout << DenganConstructor::data << endl;
+
+        }
+};
+
+class TanpaConstructor{
+    public:
+        string data;
+
+        void show(){
+            cout << "ini" << TanpaConstructor::data << endl;
+        }
+
+};
+
+int main(){
+
+    //1. Membuat object tanpa constructor
+    TanpaConstructor object1;
+    object1.data == "object 1";
+    object1.show();
+
+    //2. cara membuat object dengan constructor
+    DenganConstructor object2 = DenganConstructor("object 2");
+    object2.show();
+
+    //3. cara lain membuat object dengan constructor
+    DenganConstructor object3("object 3");
+    object3.show();
+
+    //4. cara membuat object pada heap memory(tumpukan memory). Semua yang diatas itu di stack memory
+    DenganConstructor* object4 = new DenganConstructor("object 4");
+    (*object4).show(); //dereference dulu objectnya
+
+    object4->show(); //arrow operator artinya depanya pointer yang belakang member funtion. salah satu untuk menampoilkan data pointer
+    string data = object4->data;
+    cout << data << endl;
+
+    //5. sama seperti dengan 4
+    DenganConstructor* object5;
+    object5 = new DenganConstructor("object5");
+    object5->show();
+
+    return 0;
+
+}
