3c1c3ef076227a6c74374030c332b5700306d63a
ITMaulanaali <it.maulanaali@gmail.com>
2024-09-29 21:02:32 +0700

    Berbagai macam operator java

---
+++ java/operator.java
@@ -0,0 +1,45 @@
+/*
+Berbagai jenis operator
+
+increment/decrement:
+++ dan -- //biasa digunakan untuk menambahkan satu dan sebaliknya
+
+unary operator:
++,-,~,!,(cast) //operator yang dapat digunakan pada satu operand saja
+
+perkalian/pembagian/modulus:
+*,/,% //persen adalah modulus atau sisa
+
+operator shift:
+<<,>>,>>>
+
+perbandingan:
+<,<=,>,>=,==,!=
+
+bitwise operator:
+and,or,xor
+&  ,| ,^	//digunakan untuk operator pada biner sama seperti shift
+
+kondisional:
+and, or
+&& ,||	//apabila kondisi ini terpenuhi maka statement x
+
+Ternary operator:
+? dan :
+variable = (a >= b) ? "iya a" : "iya b"
+
+assigment operator:
+=
+
+operator aritmatika:
++
+-
+*
+/
+%
+
+identifier adalah nama dari variable
+expresi adalah kombinasi operand dan operator untuk menghasilkan nilai(process)
+
+
+*/
