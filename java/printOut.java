6c33fbe1d1cd770b02fc287b8582dc6bbb0b0bb8
ITMaulanaali <it.maulanaali@gmail.com>
2024-10-07 09:55:56 +0700

    update

--- java/printOut.java
+++ java/printOut.java
@@ -1,6 +1,6 @@
 public class printOut {
     
-    public static void main(String[] args){
+    public static void main(String[] args){ //semacan input dengan type string array untuk menerima input function dari console
 
         System.out.print("Saya lana"); //melakukan print tanpa menambahkan enter pada akhirnya
         System.out.println("Saya lana kemudian enter"); //pada akhir kata di enter. alternatif \n
