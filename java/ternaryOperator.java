2c72bd526971f17a5d1e8ac31c93630a5a5ffe7d
ITMaulanaali <it.maulanaali@gmail.com>
2024-10-07 18:24:47 +0700

    mengharus statement adalah variable bervalue

---
+++ java/ternaryOperator.java
@@ -0,0 +1,12 @@
+public class ternaryOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Menggunakan ternary operator untuk menentukan nilai maksimum
+        int max = (a > b) ? a : b; 
+
+        System.out.println("Nilai maksimum adalah: " + max); //nilai yang dikeluarkan a atau 10
+    }
+    
+}
