09ff03f3b567ffc260b6add2d3c65bb1f3798fa9
ITMaulanaali <it.maulanaali@gmail.com>
2024-10-07 09:51:35 +0700

    update

---
+++ java/printOut.java
@@ -0,0 +1,9 @@
+public class printOut {
+    
+    public static void main(String[] args){
+
+        System.out.print("Saya lana"); //melakukan print tanpa menambahkan enter pada akhirnya
+        System.out.println("Saya lana kemudian enter"); //pada akhir kata di enter. alternatif \n
+        System.out.printf("Saya lana dengan nomor %d", 3); //untuk pemformartan
+    }
+}
