ae437595f7f5af58fb57d00cdaa1eab33be7fc7a
ITMaulanaali <it.maulanaali@gmail.com>
2024-10-07 10:06:02 +0700

    syntax merubah type data ke type data lain

---
+++ java/castingOperator.java
@@ -0,0 +1,14 @@
+public class castingOperator {
+
+    public static void main(String[] args){
+
+        int nilaiInt = 250;
+
+        //memperbesar rentang
+        long nilaiLong = nilaiInt;
+
+        //memperkecil rentang
+        byte nilaiByte = (byte) nilaiInt; //syntax casting. Ingat nilai dari type data yang lebih kecil di operasikan dengan nilai yang lebih besar menghasilkan nilai lebih besar
+    }
+    
+}
