3ee55ef305d1c7b2b914a0166aa7d36bf9675441
ITMaulanaali <it.maulanaali@gmail.com>
2024-10-08 20:23:56 +0700

    bermacam syntax untuk definisi array

--- java/array.java
+++ java/array.java
@@ -7,6 +7,11 @@ public class array {
         arrays[1] = 820;
         System.out.println(arrays[1]);
 
+        //syntax mirip c++
+        int arrayCpp[] = {1, 40, 80};
+        arrays[1] = 20;
+        System.out.println(arrays[1]);
+
         //Deklarasi array mirip constructor syntax tapi menggunakan type data
         String[] arrayString = new String[5]; //harus diberikan argument yang merujuk pada banyaknya koleksi data atau array
         arrayString[0] = "iya";
