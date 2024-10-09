69ac3b4453cc07a88814964263049a4a70e19c07
ITMaulanaali <it.maulanaali@gmail.com>
2024-10-10 01:10:55 +0700

    Membaca char dari file.txt

---
+++ java/PackageExercise/latihanInputOutputFile.java
@@ -0,0 +1,27 @@
+package PackageExercise;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class latihanInputOutputFile {
+    
+    public static void main(String[] args) throws IOException{
+
+        // try{
+        //     FileInputStream fileInput = new FileInputStream("input.txt");
+        // }catch(Exception e){
+        //     System.err.println(e);
+        // }finally{
+        //     if(fileInput != null){
+        //         try{
+        //             fileInput.close();
+        //         }catch(IOException e){
+        //             System.out.println("maaf ada error berikut: %s", e);
+        //         }
+        //     }
+        // }
+
+        FileInputStream fileInput = new FileInputStream("PackageExercise/input.txt"); //terdeteksi lokasi di folder java
+        System.out.println((char)fileInput.read());
+        
+    }
+}
