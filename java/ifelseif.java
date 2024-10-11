9c85e91851ef94119248438d56b415d280e2ff8b
ITMaulanaali <it.maulanaali@gmail.com>
2024-10-11 10:49:22 +0700

    if,else,elseif

--- java/ifelseif.java
+++ java/ifelseif.java
@@ -1,21 +1,48 @@
 import java.util.Scanner;
-public class ifelseif{
 
-    public static void main(String[] args){
+public class ifelseif {
 
-        Scanner inputUser = new Scanner(System.in);
 
-        int a = inputUser.nextInt();
-
-        if(/*expresion*/ a==5){
-
-            System.out.println("a itu 5");
-        }else if(a == 6){
-
-            System.out.println("input a itu 6");
-        }else{
-
-            System.out.println("tidak ada");
+    public static void main(String[] args) {
+        jika();
+        jikaSalah();
+        salahJika();
+    }
+    
+    public static void jika(){
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("masukkan nilai: ");
+        int nilai = userInput.nextInt();
+        
+        if(nilai > 70){ //eskpression
+            System.out.println("anda lulus");
         }
     }
+    
+    public static void jikaSalah(){
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("masukkan nilai: ");
+        int nilai = userInput.nextInt();
+        
+        if(nilai > 70){
+            System.out.println("anda lulus");
+        }else{
+            System.out.println("maaf anda tidak lulus");
+        }
+    }
+    
+    public static void salahJika(){
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Masukkan nilaimu: ");
+        int nilai = userInput.nextInt();
+        
+        if(nilai > 70){
+            System.out.println("selamat anda lulus");
+        }else if(nilai > 40 && nilai < 70){
+            System.out.println("coba test lagi");
+        }else{
+            System.out.print("kamu tidak lulus nilai dibawah 40");
+        }
+    }
+    
 }
