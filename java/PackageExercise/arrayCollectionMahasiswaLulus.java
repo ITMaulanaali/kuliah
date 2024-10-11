26575fe3c4548e60fa010af22245ae6df463d389
ITMaulanaali <it.maulanaali@gmail.com>
2024-10-11 23:05:44 +0700

    menambah program array untuk menyimpan nilai dan hasil untuk ditampilkan nilai dan hasil genap ganjilnya

--- java/PackageExercise/arrayCollectionMahasiswaLulus.java
+++ java/PackageExercise/arrayCollectionMahasiswaLulus.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class arrayCollectionMahasiswaLulus {
     public static void main(String[] args){
-        arrayMahasiswaLulus();
+        // arrayMahasiswaLulus();
+        ganjilGenap();
     }
 
     public static void arrayMahasiswaLulus(){
@@ -25,6 +26,29 @@ public class arrayCollectionMahasiswaLulus {
                 System.out.println("selamat untuk mahasiswa " + i + " anda lulus dengan nilai: " + nilaiMahasiswa[i]);
             }else{
                 System.out.println("Maaf karena mahasiswa " + i + " tidak lulus karena nilainya: " + nilaiMahasiswa[i]);
+            }
+        }
+        inputUser.close();
+    }
+
+    public static void ganjilGenap(){
+        //program 2 object array menyimpan nilai dan hasil untuk ditampilkan ganjil dan genap
+        Scanner inputUser = new Scanner(System.in);
+        int[] genapGanjil = new int[10];
+        int[] data = new int[10];
+        
+        for(int i=0; i<10; i++){
+            System.out.printf("Masukkan nilai Mahasiswa%d: ",i);
+            genapGanjil[i] = inputUser.nextInt();
+            
+            data[i] = genapGanjil[i] % 2;
+        }
+        
+        for(int i=0; i<10; i++){
+            if(data[i] == 0){
+                System.out.printf("Mahasiswa dengan nilai: %d adalah genap\n",genapGanjil[i]);
+            }else{
+                System.out.printf("Mahasiswa dengan nilai: %d adalah ganjil\n", genapGanjil[i]);
             }
         }
         inputUser.close();
