efe98532b62999d0dcb0fcceff1e0e821231bea9
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-10-12 23:52:52 +0700

    Update DatabasesTable.sql

--- sql/DatabasesTable.sql
+++ sql/DatabasesTable.sql
@@ -1,16 +1,16 @@
 -- Membuat table disini artinya mengcreate database baru. Syntaxnya:
-
 CREATE DATABASE databasename;
 
 
 -- Cek semua databasenya apakah ada databasename yang muncul dengan syntax:
-
 SHOW DATABASES;
 
 
 -- Syntax untuk menghapus satu database dengan mengdrop / menjatuhkan:
-
 DROP DATABASE databasename;
 
 
 --Syntax untuk melakukan backup database
+
+--Masuk ke database untuk mulai management table
+USE databasename
