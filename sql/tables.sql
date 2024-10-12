e2497a606a1d632aee50fb3a1e5ae6b6f4823788
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-10-13 00:03:44 +0700

    Create tables.sql

---
+++ sql/tables.sql
@@ -0,0 +1,10 @@
+--membuat suatu table di dalam database
+CREATE TABLE nama_tabel (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(100),
+    umur INT,
+    alamat TEXT
+);
+
+--melihat semua table yang telah dibuat
+SHOW TABLES;
