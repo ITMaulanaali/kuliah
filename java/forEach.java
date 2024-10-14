44cfb52f40a76b02796c9ee4d04388ecc3e7c18b
ITMaulanaali <it.maulanaali@gmail.com>
2024-10-15 00:41:42 +0700

    Syntax dasar untuk kemudahan looping pada data collection seperti array atau lainnya

---
+++ java/forEach.java
@@ -0,0 +1,8 @@
+public class forEach{
+	public static void main(String[] args){
+		String[] fruits = {"apple", "banana", "orange"};
+		for (String fruit : fruits) {
+    		System.out.println(fruit);
+		}
+	}
+}
