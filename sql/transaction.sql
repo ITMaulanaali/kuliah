3c7c22b540b944c400d77cc0a67c0e4d1f6bd019
ITMaulanaali <it.maulanaali@gmail.com>
2024-10-24 06:57:17 +0700

    memastikan semua query bisa di eksekusi

---
+++ sql/transaction.sql
@@ -0,0 +1,8 @@
+--Pelajari lagi transaction
+
+START TRANSACTION;
+
+--masukkan berbagai query DML
+
+COMMIT; -- untuk melanjutkan simpan ke mysql
+ROOLBACK; -- syntax mungkin salah untuk mengembalikan jika satu query gagal di eksekusi
