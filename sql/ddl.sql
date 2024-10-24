78b4b13433a765361c87e7614e9bb07e6003b25c
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-10-24 10:36:13 +0700

    pengetahuan ddl dasar.sql

---
+++ sql/ddl.sql
@@ -0,0 +1,6 @@
+--DATA DEFINITION LANGUAGE merupakan query yang digunakan untuk membuat table atau database. Untuk mendefinisikan database atau kolom-kolom yang ada
+--ada 3 perintah DDL
+--CREATE
+--DROP
+--ALTER
+
