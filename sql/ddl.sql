ab82744c5656e47522c5a201ea848fdef698a39f
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-10-24 11:45:01 +0700

    create dan referensi tabel.sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -4,8 +4,39 @@
 --DROP
 --ALTER
 
+--Behavior atau prilaku atau aturan pada reference foreign key
+--CASCADE. Ini mengizinkan kolom fk di table. Untuk ikut termanage/ubah/update jika tabel REFERENCES di tabel lain(primary key) di hapus atau diubah.
+--SET NULL. Mengizinkan table dengan kolom foreign key untuk meng set null data di kolom fk nya jika referensinya (kolom primary key di tabel lain) di update/hapus
+--SET DEFAULT. Mengizinkan table dengan kolom foreign key untuk meng set default nilai di kolomnya jika referensinya (kolom primary key di tabel lain) di update/hapus
+--RESTRICT. tidak Mengizinkan update/hapus nilai di kolomnreferensinya (kolom primary key di tabel lain yang dijadikan reference) jika masih ada atau ada referensi ke data di kolom tersebut
+--NO ACTION. 
+
+--Aturan dasar dalam membuat reference adalah type data harus benar-benar sama seperti unsigned pun harus sama perilakunya
+
 --Membuat table atau mengcreate database baru
 CREATE DATABASE databasename;
 
 --menghapus satu database dengan mengdrop / menjatuhkan:
 DROP DATABASE databasename;
+
+--Contoh dasar membuat table
+CREATE TABLE nama_table1(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nama VARCHAR(20) NOT NULL,
+  umur INT UNSIGNED,
+  golongan VARCHAR(2),
+  jurusan VARCHAR(10) NOT NULL,
+  alamat TEXT
+  ) ENGINE = InnoDB;
+
+--membuat table berelasi ke nama_table1
+CREATE TABLE nama_table2(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nama VARCHAR(20) NOT NULL,
+  umur INT UNSIGNED,
+  golongan VARCHAR(2),
+  jurusan VARCHAR(10) NOT NULL,
+  alamat TEXT,
+  fk_nama_table1 INT,
+  CONSTRAINT fk_to_nama_table1 FOREIGN KEY(fk_nama_table1) REFERENCES nama_table(id) ON DELETE CASCADE ON UPDATE CASCADE 
+  ) ENGINE = InnoDB;
