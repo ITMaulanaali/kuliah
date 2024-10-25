8c46970b55d598755eec8a5d635b2be987edf8db
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-10-25 13:45:27 +0700

    Update cmd.cmd

--- console/cmd.cmd
+++ console/cmd.cmd
@@ -1,5 +1,26 @@
-::menginstall openJDK(java Development Kita) untuk compile dan runtime versi 22
+::Setiap melakukan installasi atau perubahan data dan melakukan pengecekan ke cmd pastikan untuk ulangi buka cmd untuk refresh
+-------------------------------------------------------------------------------------------------------------------------------------
+::LAIN LAIN
+
+::melakukan cek user password wifi
+netsh wlan show profile Reno key=clear
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+:: INSTALLASI PACKAGE
+
+::menginstall netbeans(ini secara otomatis juga akan menginstall package openJDK.22 milik Bellsoft)
+winget install Apache.Netbeans
+
+::menginstall openJDK(java Development Kit) untuk compile dan runtime versi 22
 winget install Oracle.JDK.22
 
-::melakukan pencarian package java
-winget search java
+::menginstall nmap
+winget install Insecure.Nmap
+
+::menginstall speedtest cmd
+winget install Ookla.Speedtest.CLI
+
+::menginstall vscode
+winget install Microsoft.VisualStudioCode
+
+::mendownload aplikasi winbox
+winget install MikroTik.Winbox
