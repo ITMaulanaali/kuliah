c9660537c2d152ab642a5cffc7787a3b0f3c0ba5
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-11-02 16:58:35 +0700

    Update DML(DataManipulationLanguange).sql

--- sql/DML(DataManipulationLanguange).sql
+++ sql/DML(DataManipulationLanguange).sql
@@ -3,3 +3,19 @@
 --UPDATE (untuk edit data)
 --DELETE (untuk menghapus data atau record)
 
+
+--Contoh menambah data ke tabel yang memiliki 5 kolom dan mengisi semua kolomnya bisa langsung masukan valuesnya apa:
+INSERT INTO nama_table VALUES(
+  18,'iya','sekarang kita',2,'mungkin');
+
+--Contoh menambah data ke tabel yang memiliki 5 kolom dan ingin mengisi 2 baris sekaligus atau lebih:
+INSERT INTO nama_table VALUES
+  (18,'iya','sekarang kita',2,'mungkin'),
+  (19,'tidak','nanti','saya',10,'pasti');
+
+--Contoh menambah data ke tabel yang memiliki 5 kolom dan hanya ingin memasukan data di 3 kolom saja:
+INSERT INTO nama_table(
+  kolomInt,kolomVarchar,kolomText)VALUES(
+  17,'halo','aku manusia kuat');
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
