e20a25c24b8f970319ab4c016db7d78e9ce1ef40
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-11-02 19:43:27 +0700

    Update DML(DataManipulationLanguange).sql

--- sql/DML(DataManipulationLanguange).sql
+++ sql/DML(DataManipulationLanguange).sql
@@ -3,6 +3,9 @@
 --UPDATE (untuk edit data)
 --DELETE (untuk menghapus data atau record)
 
+--Setiap proses DML ini by default akan berpengaruh pada satu blok baris. Itulah mengapa pastikan untuk memberikan paramater-parameter yabg spesifik agar tidak terjadi hal yang tidak diinginkan
+
+
 
 --Contoh menambah data ke tabel yang memiliki 5 kolom dan mengisi semua kolomnya bisa langsung masukan valuesnya apa:
 INSERT INTO nama_table VALUES(
@@ -19,3 +22,32 @@ INSERT INTO nama_table(
   17,'halo','aku manusia kuat');
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--UPDATE
+
+-- Mengganti atau mengupdate semua data di semua baris yang memiliki nama_kolom_umur menjadi 20
+UPDATE nama_table SET nama_kolom_umur = 20;
+
+-- Mengganti atau update hanya data di nama_kolom_umur menjadi 19 dan nama_kolom_nama menjadi ucik disemua baris yang memiliki nama_kolom_kehidupan = manusia(blablabla)
+UPDATE nama_table
+  SET nama_kolom_umur = 20, nama_kolom_nama = 'ucik'
+  WHERE nama_kolom_kehidupan LIKE '%manusia%';
+
+UPDATE nama_table
+  SET nama_kolom_umur = 20, nama_kolom_nama = 'ucik'
+  WHERE nama_kolom_id = 4; --hanya kolom dengan id=4
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--DELETE
+
+-- Menghapus satu baris data dimana baris tersebut memiliki nama_kolom_id = 1 (satu data terhapus karena id key sifatnya unik)
+DELETE FROM nama_table WHERE nama_kolom_id = 1;
+
+-- Menghapus satu atau lebih baris data yang dibaris itu memiliki nama_kolom_jurusan = ti dan nama_kolom_nama terkandung nilai atau data ucik
+DELETE FROM nama_table WHERE nama_kolom_jurusan = 'ti' AND nama_kolom_nama LIKE '%ucik%';
+
+-- Menghapus semua data di nama_table, iya semua baris datanya hilang di nama_table akan tidak memiliki data sama sekali
+DELETE FROM nama_table;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
