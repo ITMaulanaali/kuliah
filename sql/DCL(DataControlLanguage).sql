06028d5a51372ad32d8c47f87b016256186ccc4e
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-11-07 06:41:31 +0700

    Update DCL(DataControlLanguage).sql

--- sql/DCL(DataControlLanguage).sql
+++ sql/DCL(DataControlLanguage).sql
@@ -1,3 +1,6 @@
 --Untuk mengontrol akses ke data di database. Ada 2:
 --GRANT
 --REVOKE
+--------------------------------------------------------
+-- Merubah passoword user
+ALTER USER 'username'@'lokasiUserBisaAkses(% untuk semua lokasi user ini bisa akses mysql)' IDENTIFIED BY 'new_password';
