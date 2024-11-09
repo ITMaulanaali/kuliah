f822dceabdedc757c913c4d28d3898b512400c26
ITMaulanaali <it.maulanaali@gmail.com>
2024-11-09 12:40:52 +0700

    update

--- sql/backupDatabse.sql
+++ sql/backupDatabse.sql
@@ -3,5 +3,7 @@
 sudo mysqldump -u admin -p nama_database nama_table -r /home/lan/nama_backup_database(nama_database_ini_yang_berisi_nama_table_ini).sql;
 
 
---Query untuk restore atau memasukan backup ke database nama_database_restore
-mysql -u root -p nama_database_yang_akan_di_isi_hasil_backup < /lokasi/database/format.sql;
+--Query untuk restore atau memasukan backup ke database nama_database_restore di file ./bin/mysql
+./bin/mysql -u root -p nama_database_yang_akan_di_isi_hasil_backup < /lokasi/database/format.sql;
+--Query untuk restore hasil backup dengan query di mysql
+SOURCE /lokasi/hasil/backup/di/komputer/format.sql
