3a05dda489f35a04efc1f3f688954a8a78ec60c7
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-11-09 12:31:48 +0700

    Update DCL(DataControlLanguage).sql

--- sql/DCL(DataControlLanguage).sql
+++ sql/DCL(DataControlLanguage).sql
@@ -3,6 +3,14 @@
 --REVOKE  : untuk menghapus hak akses
 --------------------------------------------------------
 -- BY DEFAULT user itu mendapatkan semua akses atau all privilage jika tidak ada set grant apapun
+--mengcreate user login di mysql. 
+--pada baris 1 admin(user) @(presentasi dari alamat user yang akses) %(bahwa semua host atau mesin atau alamat yang digunakan host dan akses sql dengan user tersebut bisa digunakan)
+--gunakan alamat ip atau localhost untuk spesfik user ini hanya bisa digunakan jika berada pada alamat ini
+--by default setelah user tercreate ia tidak dapat digunakan atau terblock semua maka dari itu berikan grant all(untuk akses semua) *(semuadatabases).*(semuatables) untuk user admin
+--flush privilages memastikan bahwa privilages grant all telah dibuat
+CREATE USER 'admin'@'%' IDENTIFIED BY 'ikanikan';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
+FLUSH PRIVILEGES; 
 
 --Pemberian hak akses selalu pada table dari database mana. Kita tidak bisa memberikan hak akses spesifik pada kolom di table karena kolom adalah satu kesatuan table
 
