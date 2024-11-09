2af17714b7f17a2dfe8c63c099947a68f3749811
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-11-09 12:26:00 +0700

    Create backupDatabse.sql

---
+++ sql/backupDatabse.sql
@@ -0,0 +1,7 @@
+--Syntax untuk melakukan backup database dengan mysqldump yang ada di direktori ./bin/mysqldump
+--		//user	/pass	/database   /table	/result-file //membuat file yang menyimpan backup
+sudo mysqldump -u admin -p nama_database nama_table -r /home/lan/nama_backup_database(nama_database_ini_yang_berisi_nama_table_ini).sql;
+
+
+--Query untuk restore atau memasukan backup ke database nama_database_restore
+mysql -u root -p nama_database_yang_akan_di_isi_hasil_backup < /lokasi/database/format.sql;
