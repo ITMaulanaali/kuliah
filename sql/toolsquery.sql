4a66fbac5d234c24a8560a6d9dfae008a8606fcb
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-11-09 12:29:30 +0700

    Delete sql/toolsquery.sql

--- sql/toolsquery.sql
+++
@@ -1,8 +0,0 @@
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- PENGELOLAAN ACCOUNT DATABASE MYSQL
-
---Syntax untuk melakukan backup database
---		//user	/pass	/database   /table	//membuat file yang menyimpan backup
-sudo mysqldump -u admin -p nama_database nama_table -r /home/lan/nama_backup_database(nama_database_ini_yang_berisi_nama_table_ini).sql;
-
-
