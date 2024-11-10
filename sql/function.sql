a01f1116180805ac56e85db427d0c568a2cdcea2
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-11-10 20:41:22 +0700

    Create function.sql

---
+++ sql/function.sql
@@ -0,0 +1,7 @@
+UPPER(nama_kolom)
+LOWER(nama_kolom)
+--sesuai namanya digunakan untuk memperbesar dan memperkecil semua nilai string di nama_kolom
+
+CONCAT(nama_kolom1, nama_kolom2) -- atau bisa kita sebut CONCAT(string1, string2)
+--Digunakan untuk mempertumakan antara 2 buah variable string gitu. atau 2 kolom tersebut masuk ke parameter fungsi concat kemudian digabung semua nilai didalamnya sehingga string antar 2 kolom melakukan pertemuan
+
