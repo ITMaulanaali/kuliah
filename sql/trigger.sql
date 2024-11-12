80d57d69970fbb360c8bea48ca1c271920814528
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-11-12 15:33:44 +0700

    Update trigger.sql

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -2,6 +2,7 @@
 --dapat melakukan trigger atau statement ketika ada suatu DML di table yang mau di buat trigger
 
 --sebelum itu buat sebuah nama table log_mhs yang berisi kolom alamat lama dan alamat baru dan kolom lainnya yang mau disini oleh delimiter
+--Membuat trigger untuk menyimpan data di table log_mhs jika ada update di table mhs yang akan diupdate sebelum perintah update dijalankan
 DELIMITER $$
 CREATE TRIGGER update_alamat_mahasiswa
   BEFORE UPDATE
