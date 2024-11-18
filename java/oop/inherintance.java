e837b2f87cfa9690c5c92963010911cce7546a3b
ITMaulanaali <it.maulanaali@gmail.com>
2024-11-18 20:59:08 +0700

    overloading, overriding, extends private

--- java/oop/inherintance.java
+++ java/oop/inherintance.java
@@ -2,25 +2,31 @@
 class ikan{
     String nama; //tidak boleh di private karena ketika di extends akan tidak terbaca di class lain
     
+    void getName(){
+        System.out.println("apen-apen nilai nama");
+    }
+    
 }
 
 class ikanKeDua extends ikan{
     //kita tidak bisa melakukan inisisalisasi nama di superclass(error) dengan this.nama. Itu sama saja seperti membuat variable string nama di class ini(membuat lagi)
     private String namaDariIkanKeDua;
     
-    public void setName(){
-        this.nama = "hai";
-        this.namaDariIkanKeDua = this.nama;
+    
+    public void setName(String i){
+        this.nama = i;
     }
     
-    public void getName(){
-        System.out.println(this.namaDariIkanKeDua);
+    public void setName(String namaAwal, String namaAkhir){ //ini disebut overloading atau nama methodnya sama dengan parameter yang berbeda
+        this.nama = namaAwal + namaAkhir;
         
     }
-}
-
-class ikanKeTiga extends ikanKeDua{
-    
+     
+    public void getName(){ //ini disebut overriding dari kategori extends yaitu menimpa atau mengganti method getName yang sudah ada(diwariskan oleh ikan) dengan getName buatan sendiri
+        System.out.println("nilai ikanKedua");
+        System.out.println(this.nama);
+        
+    }
 }
 
 
@@ -28,10 +34,17 @@ public class nyobain{
     
     public static void main(String[] args){
         
-        ikanKeTiga kuda = new ikanKeTiga();
-        kuda.setName();
+        //ini tidak menerapkan contoh overriding
+//        ikanKeTiga kuda = new ikanKeTiga(); 
+//        kuda.setName();
+//        kuda.getName();
+
+        ikanKeDua kuda = new ikanKeDua();
+        kuda.setName("aku");
         kuda.getName();
+            
     }
 }
 
 //dapat disimpulkan bahwa extends akan mewariskna data member dan function/methodnya sehingga bisa kita manipulasi dan gunakan di class lain. Mirip import tapi mungkin ini istimewa karena bisa overriding
+
