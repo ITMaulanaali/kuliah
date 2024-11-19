1689f19eba755766048f89742add79cdb45744c3
ITMaulanaali <it.maulanaali@gmail.com>
2024-11-19 09:00:24 +0700

    update

---
+++ java/oop/inheritance_Override.java
@@ -0,0 +1,31 @@
+
+class ikan{
+    
+    String nama = "dari super class ikan";
+    
+    void getName(){
+        System.out.println("Ini getName di class ikan |" + nama);
+    }
+    
+}
+
+class ikanKeDua extends ikan{
+    //mengoverride data member dari yang dimikili superclass
+    String nama = "dari subclass ikanKeDua";
+    
+    //mengoverride methode yang dimiliki superclass
+    void getName(){
+        System.out.println("ini getName di class ikanKeDua |" + nama);
+    }
+}
+
+public class nyobain{
+    public static void main(String[] args){
+        
+        ikanKeDua kuda = new ikanKeDua();
+        
+        //ini akan menggunakan dataMember atau variable serta methode dari subclass(overridenya). Sehingga method dan variable tersebut murni menggantikan milik superclass. Jika tidak di override maka proses dibawah ini akan mengambil method milik superclass
+        System.out.println(kuda.nama);
+        kuda.getName();
+    }
+}
