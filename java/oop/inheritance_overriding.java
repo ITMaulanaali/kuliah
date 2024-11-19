4614e5d06be04a0fa34c52856c9a15ea8c9cb644
ITMaulanaali <it.maulanaali@gmail.com>
2024-11-19 10:22:37 +0700

    update @Override

--- java/oop/inheritance_overriding.java
+++ java/oop/inheritance_overriding.java
@@ -14,6 +14,7 @@ class ikanKeDua extends ikan{
     String nama = "dari subclass ikanKeDua";
     
     //mengoverride methode yang dimiliki superclass
+    @Override
     void getName(){
         System.out.println("ini getName di class ikanKeDua |" + this.nama);
     }
