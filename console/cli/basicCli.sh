25ce5fd115d34436598632d2dd70c30d2c854c95
ITMaulanaali <it.maulanaali@gmail.com>
2024-12-04 00:03:46 +0700

    dasar

---
+++ console/cli/basicCli.sh
@@ -0,0 +1,33 @@
+#menampilkan siapa saya/user yang sedang kita gunakan
+whoamai
+
+#menampilkan informasi lokasi direktori kita sekarang
+pwd
+
+#menampilkan semua isi dari direktori kita sekarang
+ls
+
+#melakukan update otomatis repository
+apt-get update
+
+#melakukan upgrade package yang sudah terinstall dengan package baru yang didapat dari repository
+apt-get upgrade
+
+#membuat file baru
+touch namaFile.format
+
+#mengedit file yang sudah ada
+nano namaFile.format
+
+#berindah folder
+cd
+
+#menghapus file
+rm
+#menghapus folder beserta file didalamnya
+rm -r
+
+#mencari manual dari bin atau tool
+man rm
+
+
---
+++ console/cli/cli.sh
@@ -0,0 +1,8 @@
+#mengizinkan suatu file untuk bisa di execute
+chmod+x nama_file
+
+#melihat versi package dan letak filenya
+which mysql
+
+#menginstall mingw untuk compilasi machine code windows khususnya c++
+sudo apt install mingw-w64
