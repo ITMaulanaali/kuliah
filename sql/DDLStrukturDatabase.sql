04e2d681ec6e5415c409b42deead0b8574420fbb
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-12-03 22:23:03 +0700

    Update DDLStrukturDatabase.sql

--- sql/DDLStrukturDatabase.sql
+++ sql/DDLStrukturDatabase.sql
@@ -4,5 +4,8 @@ SHOW STATUS LIKE 'Connections';
 --Menampilkan jumlah koneksi yang terhubung saat ini
 SHOW STATUS LIKE 'Threads_connected';
 
+--Menampilkan max koneksi yang dizinkan ke database
+SHOW VARIABLES LIKE 'max_connections';
+
 --Menampilkan lebih detail user apa saja yang terkoneksi saat ini
 SHOW PROCESSLIST;
