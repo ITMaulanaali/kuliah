6d469db2008d78ed9fc8c67d5ebb79863133b848
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-12-10 09:54:00 +0700

    Update cli.sh

--- console/cli/cli.sh
+++ console/cli/cli.sh
@@ -7,6 +7,11 @@ which mysql
 #menginstall mingw untuk compilasi machine code windows khususnya c++
 apt install mingw-w64
 
+#mendownload,menambahkan repositori terbaru nodejs versi LTS ke system
+#kemudian install nodejs(include npm)
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+apt install nodejs
+
 #mengintal rar untuk esktrak windows
 apt install unrar
 
