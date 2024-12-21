4dffec1bb26a7a8219d55ed4c7b183907f3627c3
ITMaulanaali <it.maulanaali@gmail.com>
2024-12-21 12:08:23 +0700

    perintah dasar git

---
+++ git/gitCommand.sh
@@ -0,0 +1,25 @@
+#cek konfigurasi apa saja yang telah dikonfigurasi di git
+git config --list
+
+#melakukan konfigurasi minimal ke git lokal untuk bisa remote ke github ke semua repository dari git lokal(global) yaitu username dan email
+git config --global user.name "maulana"
+git config --global user.email email@gmail.com
+
+#cek sedang berada di branch mana
+git branch
+
+#melakukan remote ke github dengan ssh
+git remote add beriNamaRemotenya git@github.com:username/repository.git
+
+#memasukan hasil edit file ke staged
+git add namaFile
+
+#melakukan cek file yang ada di staged dan di modified(belum di git add)
+git status
+
+#mengcommit semua file di staged ke git lokal
+git commit -m "pesan perubahan"
+
+#mencommit atau push atau clonning commit di git lokal ke git remote(github)
+git push namaRemotenya
+
