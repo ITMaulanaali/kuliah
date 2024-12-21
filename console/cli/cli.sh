b3ccaf422d3fadbb9a50ee8e35333ac0dd0f3f4f
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2024-12-21 16:45:38 +0700

    install android studio

--- console/cli/cli.sh
+++ console/cli/cli.sh
@@ -26,3 +26,14 @@ apt install openssh_server
 #menginstall broker mosquitto dan mosquitto clients
 apt install mosquitto
 apt install mosquitto-clients
+
+#menginstall android-studio dan file serta executablenya simpan di direktori /opt/android-studio
+sudo wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2024.2.1.12/android-studio-2024.2.1.12-linux.tar.gz -P /tmp/
+sudo tar -xvzf /opt/android-studio-2024.2.1.12-linux.tar.gz -C /opt/
+rm /tmp/android-studio-2024.2.1.12-linux.tar.gz
+echo -e "[Desktop Entry]\nVersion=1.0\nName=Android Studio\nComment=Android Studio IDE\nExec=/opt/android-studio/bin/studio.sh %f\nIcon=/opt/android-studio/bin/studio.png\nTerminal=false\nType=Application\nCategories=Development;IDE;\nStartupWMClass=AndroidStudio" | sudo tee /usr/share/applications/android-studio.desktop > /dev/null
+sudo chmod +x /usr/share/applications/android-studio.desktop
+sudo chmod +x /opt/android-studio/bin/studio.sh
+update-desktop-database /usr/share/applications/
+echo "Instalasi selesai! Android Studio sudah siap digunakan."
+
