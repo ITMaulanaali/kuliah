102786bdd54b321466635f57a03d935fddd444cd
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-05 10:31:01 +0700

    type data dan variable

---
+++ kotlin/tipeData.kt
@@ -0,0 +1,45 @@
+//Integer
+Byte 8bit
+Short 16bit
+Int 32bit
+Long 64bit
+
+//Float
+Float 32bit
+Double 64bit
+
+//Hexadecimal dimulai dari:
+0x
+
+//Binary dimulai dari:
+0b
+
+//Underscore untuk memudahkan pembacaan
+100_000_000L
+
+-------------------------------------------------------------------------------------------
+//Karakter
+
+Char
+
+------------------------------------------------------------------
+//Boolean
+
+Boolean
+
+---------------------------------------------------------------------------------------
+//String
+
+//value String untuk penulisan satu baris menggunakan petik 1x
+String = "Lana"
+
+//value String untuk penulisan lebih dari satu baris menggunakan petik 3x
+String =
+"""|halo
+	|Saya
+	|Lana""".trimMargin()
+
+//template eskpressi String
+$namaVaribleString
+
+-----------------------------------------------------------------------------------------------------
---
+++ kotlin/variable.kt
@@ -0,0 +1,19 @@
+//Variable
+
+//Mutable variable (type data bisa diubah)
+var
+
+//Imutable variable (type data tidak bisa diubah)
+val
+
+//Mendefinisikan type data variable secara implisit
+var namaVariable = 30 //otomatis jadi int
+
+//Menginisialisasi type data variable secara eskplisit
+var namaVariable: Int = 30
+
+//membuat variable dengan nilai null(by default variable di kotlin tidak boleh >
+var namaVariable: Int? = null
+
+//variable Constant untuk keperluan global biasanya
+const val namaVariable: String = "hai"
