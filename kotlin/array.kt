a1268f5091b924a7f492fe7e5ec347181c7c69b9
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-05 10:47:10 +0700

    syntax dasar array

---
+++ kotlin/array.kt
@@ -0,0 +1,6 @@
+//membuat array dengan tipe data String
+val namaVariableArray: Array<String> = arrayOf("nilai1","nilai2")
+
+//membuat arrya yang boleh null
+val namaVariableArrays: Array<String?> = arrayOfNulls(5)
+namaVariableArrays[0] = "nilai1"
