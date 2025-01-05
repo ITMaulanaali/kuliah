3c8d2fa0a1be45d3c13bea19b2ab413c56dda8c4
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-05 11:09:11 +0700

    dasar operasi matematika kotlin

---
+++ kotlin/operasiMatematika.kt
@@ -0,0 +1,25 @@
+
+//kalkulator scintefic
+fun main(){
+	val namaVariable: Int = 10+10/2
+	//hasilnya adalah 15 karena sudah menggunakan kalkulator scintefic
+
+}
+
+----------------------------------------------------------
+//augmented assignment
+val namaVariable = 1
+val namaVariable1 = 10
+namaVariable += namaVariable1
+-=
+*=
+/=
+%=
+
+
+//unary operator
+namaVariable++
+--
+-
++
+!
