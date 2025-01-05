6d44357d165d5e0cbb50c5cf2e0918575bc5ad7c
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-05 12:10:32 +0700

    syntax dasar loop for

---
+++ kotlin/forLoop.kt
@@ -0,0 +1,14 @@
+//dasar looping syntax
+
+val names = arrayOf("hai","saya","lana")
+for(name in names){
+	println(name)
+}
+
+for(nilai in 0..1000){
+	println(nilai)
+}
+
+for(nilai in 0..100 step 10){
+	println(nilai)
+}
