94d05a15e3b555a631b8911f28816de98fad0841
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 17:53:34 +0700

    Operasi sisa bagi sederhana

---
+++ c++/3_operator/1_aritmatika/Modulo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    int b = 3;
+    int hasil = a % 3;
+
+    cout << "Hasil modulo atau sisa baginya: " << hasil << endl;
+
+    cin.get();
+    return 0;
+}
