b4be8b8964964dddc3e73a5b4c50cc7a3acb0afb
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 22:20:35 +0700

    Setelah syarat terpenuhi maka statement akan terlooping

---
+++ c++/5_looping/For.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int loop;
+
+    for(int a=5; a<10; a++){
+        loop += 1;
+        cout << "nilai a: " << a << endl;
+        cout << "Looping sebanyak" << loop << endl;
+    }
+}
+
+/*
+Looping for syaratnya bisa lebih diatur agar bisa menjalankan statement. Iya...
+Untuk While disebut aksi namun untuk for disebut statement
+*/
