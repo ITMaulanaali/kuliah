d837d0d33dde8ed4b5e4b64fe785fb0a1ec9d688
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 17:19:27 +0700

    pemahaman sederhana variable

---
+++ c++/1_tipeDataDanVariable/5_Variable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 20;
+    cout << a << endl;
+
+    cin.get();
+    return 0;
+}
+
+/*
+secara sederhana variable adalah tempat menampung data. Selalu bersamaan dengan type data
+Jika tanpa nilai disebut deklarasi
+Jika dengan nilai disebut inisialisasi
+
+contoh variable disini adalah a yang menampung nilai 20 bertipe int
+*/
