e4e9f0e8eddd0fd535b8034bfda3868720b69188
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 14:22:53 +0700

    Syntax dasar Int,Short,Long

---
+++ c++/1_tipeDataDanVariable/1_BilanganBulat.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    short s;
+    int i;
+    long l;
+
+    cout << sizeof(s) << "Byte" << endl;
+    cout << sizeof(i) << "Byte" << endl;
+    cout << sizeof(l) << "Byte" << endl;
+}
+
+/*
+terdapat 3 tipe data untuk bilangan bulat yaitu
+short   2Byte
+int     4Byte
+long    8Byte
+*/
