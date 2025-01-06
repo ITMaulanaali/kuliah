5ac45efb3671289c8544bb853f996527069e8a21
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 17:52:15 +0700

    Operasi sederhana +

---
+++ c++/3_operator/1_aritmatika/Penjumlahan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    int b = 2;
+    int hasil = a + b;
+
+    cout << "hasil penjumlahannya adalah: " << hasil << endl;
+
+    cin.get();
+    return 0;
+}
