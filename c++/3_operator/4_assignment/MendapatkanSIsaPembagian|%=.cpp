8f5ee487c0ae26c33f95d3e275711704a5bdb80d
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 21:56:24 +0700

    Syntax assignment untuk modulo nilai di variable

---
+++ c++/3_operator/4_assignment/MendapatkanSIsaPembagian|%=.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    a %= 2;
+
+    cout << "Hasil variable 5 operator assignment %= 2 adalah: " << a << endl;
+
+    cin.get();
+    return 0;
+}
