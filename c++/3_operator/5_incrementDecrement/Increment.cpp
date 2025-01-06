8dd0698a0d0916d9e327e2d3b66d1111ce26a485
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 21:54:10 +0700

    Menambah 1 setiap nilai numerik di variable

---
+++ c++/3_operator/5_incrementDecrement/Increment.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    a++;
+
+    cout << "Hasil variable a = 5 increment a++ adalah: " << a << endl;
+
+    cin.get();
+    return 0;
+}
+
+/*
+Pada operator increment setiap kali a++ dieksekusi itu sama saja dengan menambah satu +1 nilai di variabel a
+*/
