3cb9eba886431ba3c7faf7970c9bc1f7c8f830da
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 17:52:52 +0700

    Operasi * sederhana

---
+++ c++/3_operator/1_aritmatika/Perkalian.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    int b = 4;
+    int hasil = a * b;
+
+    cout << "Hasil perkalianya adalah: " << hasil << endl;
+
+    cin.get();
+    return 0;
+}
