1cc837325eca68b22c46f99f44bc4acca70db290
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 20:27:34 +0700

    Syntax dasar penggunaan switch case sederhana

---
+++ c++/4_percabangan/SwitchCase.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    enum Hewan {ikan,sapi,kuda,burung};
+
+    cout << "hasil tebakan benar akan menampilkan sisa hewan di enum" << endl << endl;
+    Hewan hewan = sapi;
+
+    switch(hewan){
+        case ikan:
+            cout << "ini adalah ikan kamu menebak diawal, menampilkan ikan" << endl;
+        case sapi:
+            cout << "ini adalah sapi kamu menebak dikedua, menampilkan sapi" << endl;
+        case kuda:
+            cout << "ini adalah kuda kamu menebak diketiga, menampilkan kuda" << endl;
+        case burung:
+            cout << "ini adalah burung kamu menebak diterakhir, menampilkan burung" << endl;
+        default:
+            cout << "semua hewan sudah habis" << endl;
+    }
+}
