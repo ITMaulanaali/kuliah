bb7e6bda16f1f6cdf63080ea63ce859eeb42ca4f
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 13:17:44 +0700

    perbaikan for dasar

--- c++/for.cpp
+++ c++/for.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main(){
 
-	for(int counter = 1, counter << 10, counter ++){
+	for(int counter = 1; counter < 10; counter ++){
 		cout << counter << endl;
 	}
 	cin.get();
