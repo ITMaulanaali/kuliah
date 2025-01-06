5e8cf988b60bb02c7c7b0a464b8445db69d4d12d
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 20:21:49 +0700

    syntax sederhana operator perbandingan <=

---
+++ c++/3_operator/2_perbandingan/KurangDariSamaDengan|<=.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    int b = 5;
+    int hasil = a <= b;
+
+    cout << "Hasil perbandingan 2 eskpresi adalah: " << hasil << endl;
+    
+    cin.get();
+    return 0;
+}
