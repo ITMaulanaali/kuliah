3a042ff583efb8ff328c9c0d55c92ab5acb2fbc9
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 21:54:45 +0700

    Mengurangi 1 nilai numerik di variable

---
+++ c++/3_operator/5_incrementDecrement/Decrement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    a--;
+
+    cout << "Hasil variable a = 5 decrement a-- adalah: " << a << endl;
+
+    cin.get();
+    return 0;
+}
+
+/*
+Pada operator decrement setiap kali a-- dieksekusi itu sama saja dengan mengurangi satu -1 nilai di variabel a
+*/
