9de5544f559713041f177dd132f76cd93faf2a36
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 20:23:26 +0700

    memasukan dan menampilkan data di console

---
+++ c++/2_console/InputOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string nama;
+
+    cout << "Masukkan nama anda" << endl;
+    cin >> nama;
+    cout << "Nama anda adalah: " << nama << endl;
+
+    cin.get();
+    return 0;
+}
