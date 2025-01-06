767edd2d6cb66a2a7a7f9f1ab5f3221923a84ec1
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-06 14:39:23 +0700

    float,double

---
+++ c++/1_tipeDataDanVariable/2_BilanganDecimal.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float f;
+    double d;
+
+    cout << "Float  " << sizeof(f) << "Byte" << endl;
+    cout << "Double " << sizeof(d) << "Byte" << endl;
+    cin.get();
+    return 0;
+}
+
+/*
+Tipe data float dan double adalah tipe data yang dapat menampung bilangan atau nilai berkoma(,)
+float   4Byte
+double  8Byte
+
+*/
