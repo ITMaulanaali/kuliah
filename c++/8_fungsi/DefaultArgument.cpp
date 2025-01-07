d78077b6214f21febe60274a8d398f27acdfef41
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-07 12:25:23 +0700

    Memberikan nilai default pada parameter fungsi

---
+++ c++/8_fungsi/DefaultArgument.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void luasPersegiPanjang(int nilaiA, int nilaiB = 2){
+    int hasil = nilaiA * nilaiB;
+    cout << "Hasil luas sisinya persegi panjang adalah: " << hasil << endl;
+}
+int main(){
+
+    luasPersegiPanjang(3);
+
+    cin.get();
+    return 0;
+}
+
+/*
+Default argumen secara sederhana hanyalah sebuah fungsi yang memiliki nilai default di parameternya
+*/
