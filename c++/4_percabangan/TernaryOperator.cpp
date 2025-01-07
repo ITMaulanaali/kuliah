9b718e79008b610bae792d4bc09d052e39cc0afc
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-07 18:00:15 +0700

    Mirip seperti if else dengan penulisan yang lebih sederhana dan pendek

---
+++ c++/4_percabangan/TernaryOperator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5, b = 7;
+
+    int hasil = (a < b) ? 100 : 0;
+
+    cout << hasil << endl;
+
+    cin.get();
+    return 0;
+}
+
+/*
+Ternary mirp seperti if else dengan penulisan yang lebih sederhana dan cepat
+*/
