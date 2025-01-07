2e89a5f233ca66f984adca8a5b71e64da4689c7b
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-07 13:59:49 +0700

    Syntax dasar reference

---
+++ c++/9_managementMemory/Reference.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+
+    cout << a << "\t" << &a << endl;
+
+    cin.get();
+    return 0;
+}
+
+/*
+#Reference
+a   = berisi nilai
+&a  = berisi alamat memory
+
+aPtr    = berisi alamat memory
+*Ptr    = berisi nilai
+*/
