fc163db8672a3e7c033f645839db463a24d201fe
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-07 19:28:01 +0700

    menggeser ke kiri nilai bit di satu variable

---
+++ c++/3_operator/6_bitwise/shiftLeft|<<.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main(){
+
+    int a = 100;
+
+    //geser ke kiri satu
+    int hasil = a << 1;
+
+    cout << "A = " << a << endl << endl;
+
+    cout << bitset<8>(a) << endl;
+    cout << bitset<8>(hasil) << endl << endl;
+
+    cout << "shifleft(<<) ke kiri satu dari bit variablenya adalah: " << bitset<8>(hasil) << endl;
+    cout << "Hasil bitwise << nilai bitnya jika dikonversi ke int adalah: " << hasil << endl;
+
+    cin.get();
+    return 0;
+}
