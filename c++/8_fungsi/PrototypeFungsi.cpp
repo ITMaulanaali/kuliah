4ec37614b8adaa3c4412fc40aff73ff1fdb14d2a
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-07 12:28:12 +0700

    Contoh penempatan fungsi c++ yang lebih rapi

---
+++ c++/8_fungsi/PrototypeFungsi.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//prototype fungsi. Fungsi sebenarnya ada dibawah
+void penjumlahan(int nilaiA, int nilaiB);
+void namaKu();
+
+int main(){
+
+    penjumlahan(5,5);
+    namaKu();
+
+    cin.get();
+    return 0;
+}
+
+//fungsi dengan parameter
+void penjumlahan(int nilaiA, int nilaiB){
+    int hasil = nilaiA + nilaiB;
+    cout << "penjumlahan dari keduanya adalah: " << hasil << endl;
+}
+
+//fungsi tanpa parameter
+void namaKu(){
+    cout << "Namaku adalah orang" << endl;
+}
+
+/*
+Pada pemrograman c++ itu sangat mementingkan control flow yang terstruktur dan memory. Oleh karenanya program akan dijalankan dari atas ke bawah
+Fungsi yang dipanggil namun berada di bawah tidak akan bisa dieksekusi kecuali menggunakan protoytpe function
+*/
