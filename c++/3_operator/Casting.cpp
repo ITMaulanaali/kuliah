9def4496ad8dad86e9e3f029658374e71f4d32bd
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-08 15:14:01 +0700

    syntax dasar casting atau merubah hasil type data

---
+++ c++/3_operator/Casting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float a = 3.7;
+    float b = 2.5;
+
+    int hasil = int(a+b);
+
+    cout << "Hasil casting ke int adalah: " << hasil << endl;
+
+    cin.get();
+    return 0;
+}
