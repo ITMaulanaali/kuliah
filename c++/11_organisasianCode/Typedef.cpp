20dde4128137366598cd2f3463630941a7bc3984
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-08 15:18:12 +0700

    Membuat alias nama tipe data dengan nama lain

---
+++ c++/11_organisasianCode/Typedef.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    typedef int lanaInt;
+
+    lanaInt nilai = 1;
+    cout << nilai << endl;
+
+    cin.get();
+    return 0;
+}
+
+/*
+typedef secara sederhana adalah merubah nama dari type data
+jarang digunakan
+*/
--- c++/typedef.cpp
+++
@@ -1,8 +0,0 @@
-//typedef hanyalah membuat sebuah type data memiliki alias nama yang tentunya jrang atau tidak pernah digunakan.
-
-int main(){
-	typedef int I;
-
-	I a = 5;
-	cout << a << endl; //hasilnya adalah 5 denga type data integer
-}
