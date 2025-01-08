4f1169a53f004d809adfcd65b6413b52a8909c8c
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-08 15:22:36 +0700

    type data fundamental yang bisa menyesuaikan type datanya secara otomatis tergantung nilai masukan

---
+++ c++/11_organisasianCode/Auto.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    auto a = 5;
+    auto b = "halo";
+    auto c = 4.56;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+
+    cin.get();
+    return 0;
+}
+
+/*
+auto secara sederhana adalah tipe data yang bisa otomatis mengetahui tipe dari nilainya.
+Cukup bagus jika dikombinasikan dengan template fungsi
+*/
