53fb8b5e13c55831994dd6ca853aa61fcc1cab98
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-12 21:11:32 +0700

    Contoh type data string tanpa library tambahan

---
+++ java/1_tipeDataDanVariable/StringBawaan.java
@@ -0,0 +1,12 @@
+class StringBawaan{
+
+    public static void main(String[] args){
+        String halo = "hai";
+
+        System.out.printf("String halo bernilai:     %s dengan range: tergantung banyaknya karakter",halo); System.out.println();
+    }
+}
+
+/*
+ * String -> 
+ */
