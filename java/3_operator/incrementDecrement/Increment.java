c1cf7cab78e277b205b95d3ad43f56730f2a0c32
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:14:26 +0700

    Contoh perbedaan a++ dan ++a

---
+++ java/3_operator/incrementDecrement/Increment.java
@@ -0,0 +1,16 @@
+
+class Increment {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int hasil;
+
+        hasil = a++;
+        System.out.println("Hasil variable a=5 decrement a++, simpan ke hasil: " + hasil + " nilai a: " + a);
+
+        a = 5;
+        hasil = ++a;
+        System.out.println("Hasil variable a=5 decrement ++a, simpan ke hasil: " + hasil + " nilai a: " + a);
+
+    }
+}
