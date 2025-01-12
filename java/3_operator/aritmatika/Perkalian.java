a426e68271fdc600355e3eaacac4fc64c3a211d3
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:07:55 +0700

    Contoh operasi perkalian dengan *

---
+++ java/3_operator/aritmatika/Perkalian.java
@@ -0,0 +1,11 @@
+
+class Perkalian {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int hasil = a*b;
+
+        System.out.println("Hasil perkaliannya adalah: " + hasil);
+    }
+}
