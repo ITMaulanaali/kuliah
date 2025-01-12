b718cd68bff33e635f17d9b4d072890ef6e3d115
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:02:21 +0700

    Syntax library default untuk print ke console

---
+++ java/2_console/OutputConsole.java
@@ -0,0 +1,16 @@
+class OutputConsole {
+    
+    public static void main(String[] args){
+        int a = 1;
+        float b = 1.123f;
+        String halo = "Hai";
+
+        System.out.print("Tampil ke console tanpa enter");
+        System.out.println("Tampil ke console kemudian enter");
+        System.out.printf("Tampil ke console dengan memasukan parameter int: %d float: %f String: %s",a,b,halo);
+    }
+}
+
+/*
+ * Semua ini menggunakan standart library java.util
+ */
