2087939dc921fa739b906708204b3e1ab25aac97
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:16:07 +0700

    Contoh perbandingan nilai 2 variable dengan ==

---
+++ java/3_operator/perbandingan/Sebanding.java
@@ -0,0 +1,11 @@
+
+class Sebanding {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        boolean hasil = a != b;
+
+        System.out.println("Hasil perbandingan dua eskpresi adalah: " + hasil);
+    }
+}
