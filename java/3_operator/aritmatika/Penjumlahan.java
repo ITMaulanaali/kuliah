4652eeae2821b408699c1fe841516561f8b64f81
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:05:12 +0700

    operator + untuk melakukan penjumlahan sederhana

---
+++ java/3_operator/aritmatika/Penjumlahan.java
@@ -0,0 +1,10 @@
+
+class Penjumlahan {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 2;
+        int hasil = a+b;
+
+        System.out.println("Hasil penjumlahannya adalah: " + hasil);
+    }
+}
