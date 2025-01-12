5ad08c1bfcf0d32b011bb52b8939e3aad10d3442
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 00:59:36 +0700

    Operator logika untuk membalikkan bit nilai suatu variable

---
+++ java/3_operator/logika/Not.java
@@ -0,0 +1,13 @@
+
+class Not {
+    public static void main(String[] args){
+        boolean a = true;
+        boolean b = false;
+
+        boolean logikaNot_1 = !a;
+        boolean logikaNot_0 = !b;
+
+        System.out.println("Hasil logika Not true adalah: " + logikaNot_1);
+        System.out.println("Hasil logika Not false adalah: " + logikaNot_0);
+    }
+}
