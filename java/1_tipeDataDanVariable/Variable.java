019423143cf7ecc64ba7bb4ca73e185e308da01d
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-12 21:11:54 +0700

    Apa itu variable

---
+++ java/1_tipeDataDanVariable/Variable.java
@@ -0,0 +1,13 @@
+class Variable {
+
+    public static void main(String[] args){
+        int variable = 20;
+
+        System.out.println(variable);
+    }
+
+}
+
+/*
+ * variable hanyalah nama setelah type data yang menampung nilai
+ */
