0ae8d80730f8a88505297b07812ae8928266772d
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 00:54:42 +0700

    mendapatkan nilai bit dari operasi and 2buah variable

---
+++ java/3_operator/bitwise/Or.java
@@ -0,0 +1,19 @@
+
+class Or {
+    
+    public static void main(String[] args){
+        int a = 3; String bitA;
+        int b = 6; String bitB;
+        int hasil = a | b;
+        String bitHasil;
+        
+        bitA = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+        bitB = String.format("%8s", Integer.toBinaryString(b)).replace(' ', '0');
+        bitHasil = String.format("%8s", Integer.toBinaryString(hasil)).replace(' ','0');
+
+        System.out.println("Hasil bitwise or variable a dan b adalah:");
+        System.out.println(bitA);
+        System.out.println(bitB);
+        System.out.println(bitHasil);
+    }   
+}
