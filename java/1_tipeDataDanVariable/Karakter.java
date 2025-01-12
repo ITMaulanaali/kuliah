6d3a7820432c9ae78b3b49e8622c505dd5217092
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-12 21:11:14 +0700

    contoh type data karakter dengan range 2Byte

---
+++ java/1_tipeDataDanVariable/Karakter.java
@@ -0,0 +1,12 @@
+
+class Karakter{
+    
+    public static void main(String[] args){
+        char c = 'a';
+
+        System.out.printf("Character c bernilai:   %s dengan range: %d Byte",c,Character.BYTES); System.out.println();
+    }
+}
+ /*
+  * char -> 2Byte
+  */
