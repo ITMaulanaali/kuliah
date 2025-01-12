729bb54350edd9d325e5139e0ec5e8dcb8e7604d
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-12 17:58:57 +0700

    Bilangan Decimal dan range bytesnya

---
+++ java/1_tipeDataDanVariable/BilanganDecimal.java
@@ -0,0 +1,10 @@
+class BilanganDecimal{
+    
+    public static void main(String[] args){
+        float f = 10.24f;
+        double d = 11.35;
+
+        System.out.printf("Float f bernilai:    %f dengan range: %d Byte",f, Float.BYTES); System.out.println();
+        System.out.printf("Double d bernilai:   %f dengan range: %d Byte",d, Double.BYTES); System.out.println();
+    }
+}
