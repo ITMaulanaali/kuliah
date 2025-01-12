e0850936eb3e8aa60b7dc20fe5fe20f16bbdc030
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:03:24 +0700

    operasi untuk mengambil nilai sisa hasil pembagian

---
+++ java/3_operator/aritmatika/Modulo.java
@@ -0,0 +1,11 @@
+
+class Modulo {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int hasil = a%b;
+
+        System.out.println("Hasil sisa baginya adalah: " + hasil);
+    }
+}
