e1eb774a265cd196fbabfacf3a0b7220b1ee6cbd
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:16:22 +0700

    Contoh perbandingan nilai 2 variable dengan !=

---
+++ java/3_operator/perbandingan/TidakSebanding.java
@@ -0,0 +1,11 @@
+
+class TidakSebanding {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        boolean hasil = a <= b;
+
+        System.out.println("Hasil perbandingan 2 eskpresi adalah: " + hasil);
+    }
+}
