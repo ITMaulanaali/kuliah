1c58a5411bf2dee6f2063f6e12366070678d1534
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-12 21:10:53 +0700

    Contoh type data boolean

---
+++ java/1_tipeDataDanVariable/Boolean.java
@@ -0,0 +1,14 @@
+
+class Boolean {
+
+    public static void main(String[] args){
+        boolean bo = true;
+
+        System.out.printf("Boolean bo bernilai: %b dengan range: 1 Byte\n",bo);
+    }
+    
+}
+
+/*
+ * boolean -> 1Byte
+ */
