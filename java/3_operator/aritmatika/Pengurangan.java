14c1a012692ad89da38d822901f86f34a0084d63
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:04:37 +0700

    opearsi simbol * untuk melakukan perkalian

---
+++ java/3_operator/aritmatika/Pengurangan.java
@@ -0,0 +1,11 @@
+
+class Pengurangan {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int hasil = a-b;
+
+        System.out.println("Hasil pengurangannya adalah: " + hasil);
+    }
+}
