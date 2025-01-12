da1cc7bf82ba077a5d9119a8ce2e9707b42c932d
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-12 17:58:42 +0700

    Bilangan bulat dan range bytes nya

---
+++ java/1_tipeDataDanVariable/BilanganBulat.java
@@ -0,0 +1,14 @@
+class BilanganBulat{
+    
+    public static void main(String[] args){
+        byte b = 10;
+        short s = 11;
+        int i = 12;
+        long l = 13;
+
+        System.out.printf("Byte b bernilai:     %d dengan range: %d Byte",b, Byte.BYTES); System.out.println();
+        System.out.printf("Short s bernilai:    %d dengan range: %d Byte",s, Short.BYTES); System.out.println();
+        System.out.printf("Int i bernilai:      %d dengan range: %d Byte",i, Integer.BYTES); System.out.println();
+        System.out.printf("long l bernilai:     %d dengan range: %d Byte",l, Long.BYTES); System.out.println();
+    }
+}
