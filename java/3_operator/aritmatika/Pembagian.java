bb3252ecd73004a6d58ff1190d2b55e937941814
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:04:06 +0700

    operator / untuk melakukan pembagian

---
+++ java/3_operator/aritmatika/Pembagian.java
@@ -0,0 +1,11 @@
+
+class Pembagian {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int hasil = 5/2;
+
+        System.out.println("Hasil baginya adalah: " + hasil);
+    }
+}
