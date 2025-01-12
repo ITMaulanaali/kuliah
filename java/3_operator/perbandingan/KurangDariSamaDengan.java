098cca4d58344af68d719de9bab66d4f49827e2b
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:15:18 +0700

    Contoh perbandingan nilai 2 variable dengan <=

---
+++ java/3_operator/perbandingan/KurangDariSamaDengan.java
@@ -0,0 +1,11 @@
+
+class KurangDariSamaDengan {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        boolean hasil = a <= b;
+
+        System.out.println("Hasil perbandingan dua eskpressi adalah: " + hasil);
+    }
+}
