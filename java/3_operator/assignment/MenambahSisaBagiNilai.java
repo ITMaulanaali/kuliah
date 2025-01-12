82ea77900a56b7a7854b0f9d446e786d5bb9b794
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:10:55 +0700

    menambahkan hasil sisa bagi dari pembagian suatu nilai dengan nilainya sendiri

---
+++ java/3_operator/assignment/MenambahSisaBagiNilai.java
@@ -0,0 +1,10 @@
+
+class MenambahSisaBagiNilai {
+    
+    public static void main(String[] args){
+        int a = 5;
+        a %= 2;  
+
+        System.out.println("Hasil variable 5 %= 2 adalah: " + a);
+    }  
+}
