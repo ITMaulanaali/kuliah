79ae9ac2592dfc038ce894ba0b634083b63e6541
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 01:09:18 +0700

    menambahkan hasil Membagi suatu nilai dengan nilainya sendiri

---
+++ java/3_operator/assignment/MenambahPembagianNilai.java
@@ -0,0 +1,10 @@
+
+class MenambahPembagianNilai {
+    
+    public static void main(String[] args){
+        int a = 5;
+        a /= 2;  
+
+        System.out.println("Hasil variable 5 /= 2 adalah: " + a);
+    }  
+}
