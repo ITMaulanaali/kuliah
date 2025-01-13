b3e6d693b6776fc899c0e4f74f7fec368258ce53
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 17:28:37 +0700

    Biasanya untuk menghentikan iterasi secara paksa

---
+++ java/6_controlFlow/Break.java
@@ -0,0 +1,16 @@
+
+class Break {
+
+    public static void main(String[] args) {
+        int a = 5;
+
+        while(a<=5){
+            a++;
+
+            if(a==7){
+                break;
+            }
+            System.out.println(a);
+        }
+    }
+}
