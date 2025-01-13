c94e3e75167861d46c1896c140d519a754c41c70
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 10:45:23 +0700

    Syntaxnya mirip c++, operator dengan fungsi mirip if else

---
+++ java/4_percabangan/TernaryOperator.java
@@ -0,0 +1,11 @@
+
+class TernaryOperator {
+    
+    public static void main(String[] args){
+        int a = 5, b = 2;
+
+        String hasil = (a > b) ? "Nilai a lebih besar" : "Nilai b lebih besar";
+
+        System.out.println(hasil);
+    }
+}
