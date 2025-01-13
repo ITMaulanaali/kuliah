50ff42e250ef661f2baa785043f4fcf860dfedfd
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 11:19:52 +0700

    Looping yang dideklarasikan batasnya

---
+++ java/5_looping/ForLoop.java
@@ -0,0 +1,12 @@
+
+class ForLoop {
+    
+    public static void main(String[] args) {
+        int iterasi = 0;
+
+        for(int a=5; a<=10; a++){
+            iterasi++;
+            System.out.println("Iterasi ke: " + iterasi + " Dengan a= " + a);
+        }
+    }
+}
