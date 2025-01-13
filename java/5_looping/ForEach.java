5c253dc2dcd32e4d5cc51c2626a0ba937c39f936
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 11:18:44 +0700

    Looping yang digunakan untuk menampilkan semua nilai dari array

---
+++ java/5_looping/ForEach.java
@@ -0,0 +1,11 @@
+
+class ForEach {
+    
+    public static void main(String[] args) {
+        int[] kumpulanNilai = {1,2,3,4,5};
+
+        for(int array: kumpulanNilai){
+            System.out.println(array);
+        }
+    }
+}
