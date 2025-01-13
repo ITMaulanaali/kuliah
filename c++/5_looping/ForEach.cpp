dce2e4235d75fea6251767315728fb4f1b9e75c2
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 11:19:09 +0700

    Looping khusus yang biasanya digunakan untuk menampilkan nilai dari array

---
+++ c++/5_looping/ForEach.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int kumpulanNilai[] = {10,20,30,40,50};
+
+    for(int array: kumpulanNilai){
+        cout << array << endl;
+    }
+}
