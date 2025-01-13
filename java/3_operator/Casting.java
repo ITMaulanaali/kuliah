b4010672214d29804a93d6f17bf994092f7a6012
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 10:41:03 +0700

    Merubah type data variable ke type data lain. Dengan begitu nilai akan mengikut type datanya

---
+++ java/3_operator/Casting.java
@@ -0,0 +1,17 @@
+
+class Casting {
+
+    public static void main(String[] args){
+        float a = 3.64f;
+        double b = 2.75;
+
+        double hasil = a+b;
+        System.out.println("Hasil casting ke int adalah: " + (int)hasil);
+
+    }
+
+}
+
+/*
+ * casting akan merubah variable dengan satu type data untuk dirubah ke type data lain.
+ */
