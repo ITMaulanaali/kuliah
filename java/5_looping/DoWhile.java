28ba5a2de6d6ffa2adcde29a84b999c7187ef738
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 11:17:18 +0700

    Iterasi yang setidaknya akan di loop satu kali

---
+++ java/5_looping/DoWhile.java
@@ -0,0 +1,14 @@
+
+class DoWhile {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int iterasi = 0;
+
+        do{
+            a++;
+            iterasi++;
+            System.out.println("Iterasi ke: " + iterasi + " Dengan a= " + a);
+        }while(a<=10);
+    }
+}
