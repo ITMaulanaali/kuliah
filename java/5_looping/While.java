5b4883bac2702f412118f3ca4872c5a8464b33f6
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-13 11:20:28 +0700

    Looping yang tidak dideklarasikan batasnya

---
+++ java/5_looping/While.java
@@ -0,0 +1,15 @@
+
+class While {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int iterasi = 0;
+
+        while(a<=10){
+            a++;
+            iterasi++;
+
+            System.out.println("Iterasi ke: " + iterasi + " Dengan a= " + a);
+        }
+    }
+}
