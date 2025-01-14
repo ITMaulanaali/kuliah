33c7f7bac13f6ba3f750dd72b9a31ebbdf5bfcdd
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-14 22:20:20 +0700

    public,private,default

---
+++ java/-1_oop/src/modifier/Main.java
@@ -0,0 +1,9 @@
+package modifier;
+
+class Main {
+    public static void main(String[] args) {
+        MahasiswaModifier lana = new MahasiswaModifier("lana","TI",412);
+        lana.setNim(444);
+        lana.outData();
+    }
+}
