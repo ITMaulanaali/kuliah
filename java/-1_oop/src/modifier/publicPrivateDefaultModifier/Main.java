f548e0955f103a82d8733b068c6a56d013ae6209
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-16 16:36:05 +0700

    Delete

--- java/-1_oop/src/modifier/publicPrivateDefaultModifier/Main.java
+++
@@ -1,9 +0,0 @@
-package modifier.publicPrivateDefaultModifier;
-
-class Main {
-    public static void main(String[] args) {
-        MahasiswaModifier lana = new MahasiswaModifier("lana","TI",412);
-        lana.setNim(444);
-        lana.outData();
-    }
-}
