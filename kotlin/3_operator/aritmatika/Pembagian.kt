0dafaa63105ff62ea97d624bef7ebd3fcb770a50
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-19 14:13:51 +0700

    Syntax dasar operasi aritmatika operator mirip seperti pemrograman lainnya

---
+++ kotlin/3_operator/aritmatika/Pembagian.kt
@@ -0,0 +1,9 @@
+
+fun main(){
+    
+    val a: Int = 5
+    val b: Int = 10
+    var hasil = b/a
+
+    println("Hasil baginya adalah: $hasil")
+}
---
+++ kotlin/3_operator/aritmatika/Pengurangan.kt
@@ -0,0 +1,9 @@
+
+fun main() {
+
+    val a: Int = 5
+    val b: Int = 2
+    var hasil = a-b
+
+    println("Hasil pengurangannya adalah: $hasil")
+}
