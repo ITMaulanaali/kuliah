d6833f8072f36751932df3ead51aada6479c37d4
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-19 18:15:28 +0700

    jenis jenis variable dasar

--- kotlin/1_tipeDataDanVariable/Variable.kt
+++ kotlin/1_tipeDataDanVariable/Variable.kt
@@ -1,17 +1,21 @@
 
 fun main() {
 
-    val imutable: Int = 10
+    val imutable: Int? = 10
     var mutable: Int = 11
     mutable = 12
 
     val variableTypeDataOtomatis = 20.5
     val variableDapatNull: Int? = null
 
+    //mengkonversi dari variable null ke not null dengan !!
+    val variableNotNullAssertion: Int = imutable!!
+
     println("NIlai imutable:      $imutable")
     println("NIlai mutable:       $mutable")
     println("NIlai variableNull:  $variableDapatNull")
     println("NIlai variableNull:  $variableTypeDataOtomatis")
+    println("Nilai konversi variable Null ke tidak Null dari variable imutable ${variableNotNullAssertion}")
 }
 
 /*
@@ -21,6 +25,9 @@ terdapat 2 jenis variable:
 
 - mutable(dapat berubah): var
     variable yang seperti biasanya dapat menyimpan nilai secara dinamis
+
+Variable akan secara otomatis mengembalikan nullException jika ada kesalahan antara variable yang dapat null dengan yang tidak dapat null
+Olehh karena itu bisa melakukan perubahan variable dengan Not Null
 
 Direkomendasikan untuk selalu membuat variable dengan val
  */
