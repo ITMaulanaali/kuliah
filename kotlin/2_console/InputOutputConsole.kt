9e1f391d7f6eb4853b6fc0026d5faa6712093ce6
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-19 13:39:13 +0700

    nilai dengan typedata nullable juga harus diassignt ke type data yang dapat nullable

---
+++ kotlin/2_console/InputOutputConsole.kt
@@ -0,0 +1,14 @@
+
+fun main(){
+
+    var inputUser: String?
+
+    println("Masukkan namamu: ")
+    inputUser = readLine();
+    println("Namamu adalah: $inputUser")
+}
+
+/*
+fungsi readLine() akan mengembalikan nilai inputan String yang dapat null String?
+kemudian kita assignt nilai yang dapat null ke inputUser yang juga typedata yang sama String?
+*/
