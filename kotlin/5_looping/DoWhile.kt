8bbc4d7fa23b5072f6b7a18797091c0051c75f11
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-20 00:11:09 +0700

    DoWhile setidaknya melakukan sekali

---
+++ kotlin/5_looping/DoWhile.kt
@@ -0,0 +1,12 @@
+
+fun main(){
+
+    var a: Int = 5
+    var iterasi: Int = 0
+
+    do{
+        a++;
+        iterasi++;
+        println("Iterasi ke:    $iterasi Dengan a= $a")
+    }while(a<=10)
+}
---
+++ kotlin/5_looping/While.kt
@@ -0,0 +1,12 @@
+
+fun main(){
+
+    var a: Int = 5
+    var iterasi: Int = 0
+
+    while(a<=10){
+        a++;
+        iterasi++;
+        println("Iterasi ke:    $iterasi Dengan a= $a")
+    }
+}
