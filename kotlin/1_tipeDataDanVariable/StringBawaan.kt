f32d7d79d13b08f3b7501d53479a5cdc60c67678
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-20 21:17:25 +0700

    Syntax dasar String

--- kotlin/1_tipeDataDanVariable/StringBawaan.kt
+++ kotlin/1_tipeDataDanVariable/StringBawaan.kt
@@ -2,8 +2,13 @@
 fun main() {
 
     val s: String = "haloString"
-    
     println("String s bernilai: \"$s\" dengan range: tergantung banyaknya karakter")
+
+    //value String untuk penulisan lebih dari satu baris menggunakan petik 3x
+    String =
+ """|halo
+	|Saya
+	|Lana""".trimMargin()
 }
 
 /*
