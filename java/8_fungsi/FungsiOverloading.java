94ca9e1ac119dd7ed145bc5ab75d77155238c2a5
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-20 21:06:44 +0700

    Disebut sebagai overloading karena kegunaanya harusnya untuk overload parameternya bukan typedata fungsinya

--- java/8_fungsi/FungsiOverloading.java
+++ java/8_fungsi/FungsiOverloading.java
@@ -2,20 +2,25 @@
 class FungsiOverloading {
     
     public static void main(String[] args) {
-        System.out.println(luasPersegi(2,5));
+        System.out.println(volumePersegi(2,5));
     }
 
-    static int luasPersegi(int panjang, int lebar){
-        int luas = panjang * lebar;
+    static int volumePersegi(int panjang, int lebar, int tinggi){
+        int luas = panjang * lebar * tinggi;
         return luas;
     }
     
-    static void luasPersegi(int sisi){
-        int luas = sisi * sisi;
-        System.out.println("Hasil luasnya void adalah: ");
+    static void volumePersegi(int panjang){
+        int luas = panjang * panjang * panjang;
+        System.out.println("Hasil luasnya void adalah: " + luas);
     }
     
-    static double luasPersegi(double sisi){
-        return sisi * sisi;
+    static double volumePersegi(double panjang){
+        return panjang * panjang * panjang;
     }
 }
+
+/**
+ * Overloading dapat dilakukan hanya dengan karena return typenya berbeda. Yang harusnya tidak boleh dilakukan karena tujuan overloading adalah berdasarkan beda beda parameternya
+ * Overriding tidak dapat dilakukan dengan return type yang berbeda
+ */
