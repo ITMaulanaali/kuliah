5514dee53a3c28f2b32da543dddda565ac52a7ec
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-20 21:25:07 +0700

    Kotlin mendukung penyebutan nama argument yang akan di isi

---
+++ kotlin/8_fungsi/NamedArgument.kt
@@ -0,0 +1,14 @@
+
+fun main(){
+
+    penjumlahanPersegi(tinggi=10,panjang=5,lebar=2)
+}
+
+fun penjumlahanPersegi(panjang: Int, lebar: Int, tinggi: Int){
+    val hasil: Int = panjang + lebar + tinggi
+    println("Hasil penjumlahanya adalah: $hasil")
+}
+
+/**
+maksud dari named argument adalah argument yang urutanya bisa disebut atau balik balik saat pemanggilan
+ */
