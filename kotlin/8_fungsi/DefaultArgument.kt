4c31a792a8ed9491587318286b42c455ede49503
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-20 21:29:49 +0700

    Contoh syntax default argument yang bisa null

--- kotlin/8_fungsi/DefaultArgument.kt
+++ kotlin/8_fungsi/DefaultArgument.kt
@@ -4,7 +4,7 @@ fun main(){
     penjumlahanPersegi(5,5)
 }
 
-fun penjumlahanPersegi(panjang: Int, lebar: Int, tinggi: Int = 2){
-    val hasil: Int = panjang + lebar + tinggi
+fun penjumlahanPersegi(panjang: Int, lebar: Int? = null, tinggi: Int = 2){
+    val hasil: Int = panjang + tinggi
     println("Hasil penjumlahanya adalah: $hasil")
 }
