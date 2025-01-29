470a067cf9e3dc02468100d4893cb7a9db9bb0c0
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-29 22:04:34 +0700

    Tambahi untuk input data dari console

---
+++ javaScript/2_console/InputOutput.js
@@ -0,0 +1,9 @@
+let nama = "lana";
+let nim = 412;
+
+console.log(`Anda adalah: ${nama} dengan nim: ${nim}`);
+
+/**
+ * Untuk menyisipkan data ke dalam string harus didalam template literal(ini berbeda dengan string). Untuk membuat template literal menggunakan backtiks`
+ * lanjutkan lagi untuk input datanya
+ */
