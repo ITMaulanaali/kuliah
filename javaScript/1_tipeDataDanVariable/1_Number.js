ca864daec58bd8377197c85fe8a1e0af9fbe0cbe
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-29 22:03:18 +0700

    Tipe data numerik di javascript adalah hanya numerik dengan rentang 8Byte

---
+++ javaScript/1_tipeDataDanVariable/1_Number.js
@@ -0,0 +1,14 @@
+let numberInt = 10;
+let numberMin = -10;
+let numberBigInt = 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000n;
+let numberPecahan = 1.45;
+
+console.log(numberInt);
+console.log(numberMin);
+console.log(numberBigInt);
+console.log(numberPecahan);
+
+/**
+ * JavaScript menggunakan alokasi 64bit/8byte untuk number
+ * tapi tidak terbatas untuk type data BigInt dengan memberikan n dibelakang angka
+ */
