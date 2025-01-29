b47ed489d1f603bc37e7bc23422117d974202248
ITMaulanaali <it.maulanaali@gmail.com>
2025-01-29 22:04:21 +0700

    type data di javascript tidak ditulis secara eskplisit sehingga null dan undefined termasuk data yang bertipe

---
+++ javaScript/1_tipeDataDanVariable/4_Null_Undefined.js
@@ -0,0 +1,10 @@
+let nullTypeData = null;
+let undefinedVar = undefined;
+
+console.log(nullTypeData);
+console.log(undefinedVar);
+
+/**
+ * null berarti type data kosong yang benar benar type data dengan definisi nilai kosong
+ * sedangkan undefined mengartikan data yang memang tidak ada isinya  dan tidak terdefinisi
+ */
