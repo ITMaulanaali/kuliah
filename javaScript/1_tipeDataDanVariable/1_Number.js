4ee23cf58bfb422a5308d7ed848887f3d4236540
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2025-02-06 01:22:31 +0700

    Update 1_Number.js

--- javaScript/1_tipeDataDanVariable/1_Number.js
+++ javaScript/1_tipeDataDanVariable/1_Number.js
@@ -1,12 +1,14 @@
-let numberInt = 10;
+let numberInt = 10; //Number desimal hanya dapat maksimal 15 digit
 let numberMin = -10;
 let numberBigInt = 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000n;
 let numberPecahan = 1.45;
+let numberEskponen = 123e5;
 
 console.log(numberInt);
 console.log(numberMin);
 console.log(numberBigInt);
 console.log(numberPecahan);
+console.log(numberEskponen);
 
 /**
  * JavaScript menggunakan alokasi 64bit/8byte untuk number
