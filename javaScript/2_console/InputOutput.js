ab65089b9ba4261b99fa09f542c8c21826bedb93
ITMaulanaali <it.maulanaali@gmail.com>
2025-02-08 21:41:50 +0700

    Melakukan input dan output di notif v8 engine browser

--- javaScript/2_console/InputOutput.js
+++ javaScript/2_console/InputOutput.js
@@ -1,9 +1,12 @@
-let nama = "lana";
-let nim = 412;
+let nama;
+let nim;
 
-console.log(`Anda adalah: ${nama} dengan nim: ${nim}`);
+nama = prompt("Masukkan nama");
+nim = prompt("Masukkan Nim");
+
+console.log(`Anda adalah: ${nama} dengan nim: ${nim}`)
 
 /**
  * Untuk menyisipkan data ke dalam string harus didalam template literal(ini berbeda dengan string). Untuk membuat template literal menggunakan backtiks`
- * lanjutkan lagi untuk input datanya
+ * ada juga alert() dan confirm()
  */
