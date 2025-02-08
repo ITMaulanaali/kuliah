f98f73271f2d7e06527bc1f9d5779cfc0850da33
ITMaulanaali <it.maulanaali@gmail.com>
2025-02-08 21:42:40 +0700

    Built in fungsi untuk parsing ke type data lain

---
+++ javaScript/3_operator/Casting.js
@@ -0,0 +1,13 @@
+let a = 3.7;
+let b = 1;
+let c = "22.456";
+
+let hasilNumberInt = parseInt(c);
+let hasilNumberFloat = parseFloat(c);
+let hasilString = String(a);
+let hasilBoolean = Boolean(b);
+
+console.log("Hasil casting string ke int adalah: " + hasilNumberInt);
+console.log("Hasil casting string ke float adalah: " + hasilNumberFloat);
+console.log("Hasil casting number ke string adalah: " + hasilString);
+console.log("Hasil casting number ke boolean adlaah: " + hasilBoolean);
