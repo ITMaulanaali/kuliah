f727fd361ab3b4acee0548a5f6783024e644c7bf
ITMaulanaali <it.maulanaali@gmail.com>
2025-02-08 21:43:11 +0700

    Cara lebih sedehana untuk mengoperasikan dalam satu baris

---
+++ javaScript/3_operator/Comma.js
@@ -0,0 +1,9 @@
+let a,b,c;
+a = (b=5,c=2,b+c);
+
+console.log(a);
+
+/*
+Comma operator dapat mempersingkat dan mengecilkan code program
+contohnya ketika ingin mengassign nilai penjumlahan b+c ke a
+*/
