125e022983cd6c6dbf5f9f119ac783af6abc0569
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2025-03-13 06:09:39 +0700

    tata query trigger

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -4,13 +4,10 @@
 --sebelum itu buat sebuah nama table log_mhs yang berisi kolom alamat lama dan alamat baru dan kolom lainnya yang mau disini oleh delimiter
 --Membuat trigger untuk menyimpan data di table log_mhs jika ada update di table mhs yang akan diupdate sebelum perintah update dijalankan
 DELIMITER $$
-CREATE TRIGGER update_alamat_mahasiswa
-  BEFORE UPDATE
-  ON nama_table_mahasiswa
-  FOR EACH ROW
+CREATE TRIGGER update_alamat_mahasiswa BEFORE UPDATE
+  ON nama_table_mahasiswa FOR EACH ROW
 BEGIN
-  INSERT INTO log_mhs(
-  nim, alamat_lama, alamat_baru, waktu)VALUES(
-  OLD.nim, OLD.alamat_lama, NEW.alamat_baru, NOW());
+  INSERT INTO log_mhs(nim, alamat_lama, alamat_baru, waktu) VALUES
+  (OLD.nim, OLD.alamat_lama, NEW.alamat_baru, NOW());
   END $$
 DELIMITER;
