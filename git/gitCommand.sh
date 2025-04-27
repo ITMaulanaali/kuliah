fd3a62b2e3ee22205d82b151d971ff7b4d1aca5b
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2025-04-27 08:02:03 +0700

    Update gitCommand.sh

--- git/gitCommand.sh
+++ git/gitCommand.sh
@@ -66,14 +66,14 @@ git stash pop
 #mengupdate data branch yang terbaru di github ke git local
 git pull --prune
 
-#menghapus semua file dan folder yang masih untracked
-git clean -fd
-
-#menghapus file dari track
-git rm namaFile.txt
+#menghapus atau mengembalikan perubahan file dari yang baru ke awal saat commit terakhir
+git restore namaFile.txt
 
 #memindahkan file dari track menjadi untracked
 git rm --cached
+
+#menghapus semua file dan folder yang masih untracked
+git clean -fd
 
 #melakukan merge dari branch lain
 #fast-forward
