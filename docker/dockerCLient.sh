0a0e7b75e9209da1d112df37548bd105aef11d4d
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2025-06-07 11:45:59 +0700

    docker dasar

---
+++ docker/dockerCLient.sh
@@ -0,0 +1,9 @@
+#melihat kumpulan image di registry kita
+docker image ls
+
+#mendownload image dengan versi paling baru dari docker hub ke registry
+docker image pull ubuntu:latest
+
+#menghapus image ubuntu yang tag nya latest
+docker image rm ubuntu:latest
+
