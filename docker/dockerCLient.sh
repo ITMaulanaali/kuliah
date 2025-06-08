43dd309a4912f191a1b06408ec48e0cea911c9bc
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2025-06-08 07:21:16 +0700

    Update dockerCLient.sh

--- docker/dockerCLient.sh
+++ docker/dockerCLient.sh
@@ -1,4 +1,7 @@
-#melihat kumpulan image di registry kita
+#mengambil informasi versi dari docker engine
+docker version
+
+#melihat kumpulan image yang sudah dibuat atau download
 docker image ls
 
 #mendownload image dengan versi paling baru dari docker hub ke registry
@@ -7,3 +10,16 @@ docker image pull ubuntu:latest
 #menghapus image ubuntu yang tag nya latest
 docker image rm ubuntu:latest
 
+#melihat semua container yang ada
+docker container ls -a
+#melihat semua container yang sedang berjalan
+docker container ls
+
+#membuat container baru dengan nama latihan
+docker container create --name latihan ubuntu:latest
+
+#mengstop container agar berhenti berjalan
+docker container stop namaContainer/id
+
+#menghapus container
+docker container rm namaContainer/id
